9f1b0770175ac11db2131809837610813c7f62c4
Drycheetah84 <legal.gorillarealism@gmail.com>
2025-12-25 12:29:52 -0800

    Add all missing response model classes

--- NullStack/Runtime/Models/NullStackModels.cs
+++ NullStack/Runtime/Models/NullStackModels.cs
@@ -196,3 +196,371 @@ namespace NullStack.Models
         public object details;
     }
 }
+
+    // Additional Response Models
+    [Serializable]
+    public class GenericResponse
+    {
+        public bool success;
+        public string message;
+    }
+
+    [Serializable]
+    public class RegisterResponse
+    {
+        public bool success;
+        public LoginData data;
+    }
+
+    [Serializable]
+    public class CustomIdLoginRequest
+    {
+        public string customId;
+        public bool createAccount;
+        public string titleId;
+    }
+
+    [Serializable]
+    public class PlayerProfileResponse
+    {
+        public bool success;
+        public PlayerData data;
+    }
+
+    [Serializable]
+    public class UpdateProfileRequest
+    {
+        public string displayName;
+    }
+
+    [Serializable]
+    public class PlayerDataResponse
+    {
+        public bool success;
+        public PlayerDataContainer data;
+    }
+
+    [Serializable]
+    public class PlayerDataContainer
+    {
+        public PlayerDataItem[] data;
+    }
+
+    [Serializable]
+    public class PlayerDataItem
+    {
+        public string key;
+        public string value;
+    }
+
+    [Serializable]
+    public class UpdatePlayerDataRequest
+    {
+        public PlayerDataItem[] data;
+    }
+
+    [Serializable]
+    public class PlayerStatisticsResponse
+    {
+        public bool success;
+        public PlayerStatisticsData data;
+    }
+
+    [Serializable]
+    public class PlayerStatisticsData
+    {
+        public StatisticValue[] statistics;
+    }
+
+    [Serializable]
+    public class StatisticValue
+    {
+        public string name;
+        public int value;
+    }
+
+    [Serializable]
+    public class StatisticUpdate
+    {
+        public string name;
+        public int value;
+    }
+
+    [Serializable]
+    public class UpdateStatisticsRequest
+    {
+        public StatisticUpdate[] statistics;
+    }
+
+    [Serializable]
+    public class CloudScriptListResponse
+    {
+        public bool success;
+        public CloudScriptListData data;
+    }
+
+    [Serializable]
+    public class CloudScriptListData
+    {
+        public string[] functions;
+    }
+
+    [Serializable]
+    public class CloudScriptHistoryResponse
+    {
+        public bool success;
+        public CloudScriptHistoryData data;
+    }
+
+    [Serializable]
+    public class CloudScriptHistoryData
+    {
+        public CloudScriptExecution[] executions;
+    }
+
+    [Serializable]
+    public class CloudScriptExecution
+    {
+        public string id;
+        public string functionName;
+        public string executedAt;
+        public object result;
+    }
+
+    [Serializable]
+    public class UpdateLeaderboardResponse
+    {
+        public bool success;
+        public LeaderboardUpdateData data;
+    }
+
+    [Serializable]
+    public class LeaderboardUpdateData
+    {
+        public int position;
+        public int value;
+    }
+
+    [Serializable]
+    public class PlayerRankResponse
+    {
+        public bool success;
+        public PlayerRankData data;
+    }
+
+    [Serializable]
+    public class PlayerRankData
+    {
+        public int position;
+        public int value;
+        public string displayName;
+    }
+
+    [Serializable]
+    public class PlayerCurrencyResponse
+    {
+        public bool success;
+        public PlayerCurrencyData data;
+    }
+
+    [Serializable]
+    public class PlayerCurrencyData
+    {
+        public CurrencyBalance[] currencies;
+    }
+
+    [Serializable]
+    public class CurrencyBalance
+    {
+        public string currencyCode;
+        public int amount;
+    }
+
+    [Serializable]
+    public class CurrencyChangeRequest
+    {
+        public string currencyCode;
+        public int amount;
+    }
+
+    [Serializable]
+    public class CatalogItemsResponse
+    {
+        public bool success;
+        public CatalogItemsData data;
+    }
+
+    [Serializable]
+    public class CatalogItemsData
+    {
+        public CatalogItem[] items;
+    }
+
+    [Serializable]
+    public class PlayerInventoryResponse
+    {
+        public bool success;
+        public PlayerInventoryData data;
+    }
+
+    [Serializable]
+    public class PlayerInventoryData
+    {
+        public InventoryItem[] items;
+    }
+
+    [Serializable]
+    public class InventoryItem
+    {
+        public string instanceId;
+        public string itemId;
+        public int quantity;
+    }
+
+    [Serializable]
+    public class PurchaseItemResponse
+    {
+        public bool success;
+        public PurchaseItemData data;
+    }
+
+    [Serializable]
+    public class PurchaseItemData
+    {
+        public string instanceId;
+        public int remainingBalance;
+    }
+
+    [Serializable]
+    public class ConsumeItemRequest
+    {
+        public string instanceId;
+        public int consumeCount;
+    }
+
+    [Serializable]
+    public class ConsumeItemResponse
+    {
+        public bool success;
+        public ConsumeItemData data;
+    }
+
+    [Serializable]
+    public class ConsumeItemData
+    {
+        public int remainingUses;
+    }
+
+    [Serializable]
+    public class GrantItemRequest
+    {
+        public string itemId;
+    }
+
+    [Serializable]
+    public class GrantItemResponse
+    {
+        public bool success;
+        public GrantItemData data;
+    }
+
+    [Serializable]
+    public class GrantItemData
+    {
+        public string instanceId;
+    }
+
+    [Serializable]
+    public class CreateMatchmakingTicketRequest
+    {
+        public string queueName;
+        public Dictionary<string, object> attributes;
+    }
+
+    [Serializable]
+    public class MatchDetailsResponse
+    {
+        public bool success;
+        public MatchDetailsData data;
+    }
+
+    [Serializable]
+    public class MatchDetailsData
+    {
+        public string matchId;
+        public string[] playerIds;
+        public string status;
+    }
+
+    [Serializable]
+    public class ActiveMatchesResponse
+    {
+        public bool success;
+        public ActiveMatchesData data;
+    }
+
+    [Serializable]
+    public class ActiveMatchesData
+    {
+        public MatchInfo[] matches;
+    }
+
+    [Serializable]
+    public class MatchInfo
+    {
+        public string matchId;
+        public string status;
+        public int playerCount;
+    }
+
+    [Serializable]
+    public class TrackEventRequest
+    {
+        public string eventName;
+        public Dictionary<string, object> eventData;
+    }
+
+    [Serializable]
+    public class CustomEventRequest
+    {
+        public string eventName;
+        public string eventType;
+        public Dictionary<string, object> properties;
+    }
+
+    [Serializable]
+    public class PlayerEventsResponse
+    {
+        public bool success;
+        public PlayerEventsData data;
+    }
+
+    [Serializable]
+    public class PlayerEventsData
+    {
+        public PlayerEvent[] events;
+    }
+
+    [Serializable]
+    public class PlayerEvent
+    {
+        public string eventName;
+        public string timestamp;
+        public object data;
+    }
+
+    [Serializable]
+    public class AnalyticsSummaryResponse
+    {
+        public bool success;
+        public AnalyticsSummaryData data;
+    }
+
+    [Serializable]
+    public class AnalyticsSummaryData
+    {
+        public int totalEvents;
+        public int uniquePlayers;
+        public Dictionary<string, int> eventCounts;
+    }
+}
